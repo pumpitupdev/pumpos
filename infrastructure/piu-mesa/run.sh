deda764f01db4a7821b2c7a39033bea5674e9cb1
icex2 <djh.icex2@gmail.com>
2020-06-12 17:05:00 +0200

    wip

---
+++ infrastructure/piu-mesa/run.sh
@@ -0,0 +1,20 @@
+#xhost +local:root #Needed if using hosts X display
+
+if [ -z "$1" ]
+  then
+    echo "No data path supplied"
+    echo "Usage example for pumpos: ./runnmesa.sh /piu/data/17_zero/"
+    exit -1
+fi
+
+docker run \
+    --privileged \
+    -v /dev/bus/usb:/dev/bus/usb \
+    --device /dev/dri \
+    --device /dev/vga_arbiter \
+    -it \
+    --rm \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v "$1":/piu \
+    -e DISPLAY="$DISPLAY" \
+    --name piu-mesa pumpos:piu-mesa
---
+++ infrastructure/piu-nvidia/build.sh
@@ -0,0 +1,15 @@
+MAGE_NAME=ubuntu_with_nvidia_driver
+NVIDIA_DRIVER=NVIDIA-Linux-x86_64-*.run  # path to nvidia driver
+
+nvidia_host_version() {
+    echo "$(head -n1 </proc/driver/nvidia/version | cut -d' ' -f 9)"
+}
+
+
+nvidia_url() {
+    echo "https://http.download.nvidia.com/XFree86/Linux-x86_64/$(nvidia_host_version)/NVIDIA-Linux-x86_64-$(nvidia_host_version).run"
+}
+
+[ -f "NVIDIA-DRIVER.run" ] || wget $(nvidia_url) -O NVIDIA-DRIVER.run || echo "Please download nvidia driver with same version as the host. Probably $(nvidia_host_version). Guessed URL $(nvidia_url) but it did not work"
+
+docker build -f Dockerfile -t piu .
