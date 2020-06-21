731526ed4ed9edcf4b92eed00f58e370896806a9
icex2 <djh.icex2@gmail.com>
2020-06-21 18:55:04 +0200

    dist/data/run.sh: Bugfix, set game root working directory
    This was pointed out as an issue in the past but I didn't
    realize that this is only an issue on hook.conf entries with
    relative paths instead of absolute ones.
    
    Fix by switching to the game version's root dir on every game.

--- dist/piu/data/27_pri/run.sh
+++ dist/piu/data/27_pri/run.sh
@@ -3,4 +3,5 @@
 # Root path is path of this script
 root_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+cd "$root_path" || exit 1
 "$root_path/cabinet-startx.sh" ./xorg.conf/xorg-fx-720.conf 720 48
